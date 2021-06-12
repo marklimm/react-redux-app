4c7032db1cfd296bbe7d37a1c5bc7bfc82f456ce
Mark <mark.limm@gmail.com>
2021-06-12 12:12:31 -0400

    completion notes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
 import { Provider } from 'react-redux'
 
 import { Consumer } from 'components/Consumer/Consumer'
